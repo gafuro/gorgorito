40d187a6eea8aed16556d7aa1748053e567e44d9
Gabriel Furet <git push -u origin master>
2021-01-06 11:27:56 +0200

    using sqlite instead of local db

--- tests/Feature/FollowUserTest.php
+++ tests/Feature/FollowUserTest.php
@@ -3,34 +3,36 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 use App\User;
 
 class FollowUserTest extends TestCase
 {
-    use DatabaseMigrations;
+    use DatabaseMigrations, WithFaker, RefreshDatabase;
 
     /** @test */
     function followedUsersGetNotification()
     {
         $authUser = User::create([
-            'username' => 'u1',
-            'name' => 'user 1',
-            'email' => 'u1@test.com',
+            'username' => $this->faker->userName,
+            'name' => $this->faker->name,
+            'email' => $this->faker->email,
             'password' => Hash::make('password')
         ]);
 
         auth()->login($authUser);
 
         $user2 = User::create([
-            'username' => 'u2',
-            'name' => 'user 2',
-            'email' => 'u2@test.com',
+            'username' => $this->faker->userName,
+            'name' => $this->faker->name,
+            'email' => $this->faker->email,
             'password' => Hash::make('password')
         ]);
 
-        $authUser->follow($user2);
+        $this->post(route('follow',$user2));
 
         $this->assertCount(1, $user2->notifications);
 
